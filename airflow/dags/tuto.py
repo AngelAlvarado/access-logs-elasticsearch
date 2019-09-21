c2567b0cb2023d824f51fc7cf0f6fd875b083d57
Angel Alvarado <angel.angelio@gmail.com>
2019-09-20 19:53:27 -0700

    reading file

--- airflow/dags/tuto.py
+++ airflow/dags/tuto.py
@@ -4,7 +4,10 @@ http://airflow.readthedocs.org/en/latest/tutorial.html
 """
 from airflow import DAG
 from airflow.operators.bash_operator import BashOperator
+from airflow.operators.python_operator import PythonOperator
 from datetime import datetime, timedelta
+import re
+import os
 
 
 default_args = {
@@ -21,6 +24,30 @@ default_args = {
     # 'priority_weight': 10,
     # 'end_date': datetime(2016, 1, 1),
 }
+
+def write_logs():
+    #load Directory
+    logs = os.listdir("airflow/data/apache_logs")
+    #Define regex by log line
+    regex = '^(\S+) (\S+) (\S+) \[([\w:/]+\s[+\-]\d{4})\] "(\S+) (\S+)\s*(\S+)\s*" (\d{3}) (\S+)'
+    #Read file log of directory
+    for i in range(len(logs)):
+        path_log =  "airflow/data/apache_logs/" + logs[i]
+        f = open(path_log, "r")
+        #Read Lines
+        for line in f:
+            if re.match(regex, line):
+                match =  re.search(regex, line).groups()
+                host          = match[0]
+                client_identd = match[1]
+                user_id       = match[2]
+                date_time     = match[3]
+                method        = match[4]
+                endpoint      = match[5]
+                protocol      = match[6]
+                response_code = int(match[7])
+                content_size  = match[8]
+        f.close()
 
 dag = DAG("tutorial", default_args=default_args, schedule_interval=timedelta(1))
 
@@ -43,6 +70,12 @@ t3 = BashOperator(
     params={"my_param": "Parameter I passed in"},
     dag=dag,
 )
+
+t4 = PythonOperator(
+    task_id='write_elastic_serach',
+    provide_context=True,
+    python_callable=write_logs,
+    dag=dag,
 
 t2.set_upstream(t1)
 t3.set_upstream(t1)
